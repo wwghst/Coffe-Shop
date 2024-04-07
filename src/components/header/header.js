a3cb8f96fb998513f5ad7a301c303890c0e65af2
Arslan <wowghost@bk.ru>
2024-04-07 20:46:22 +0600

    Header Layout Done

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,1 +1,28 @@
+import { Component } from 'react';
+
+import LogoOne from '../../assets/LogoOne.svg';
+
 import './header.scss';
+
+class Header extends Component {
+  render() {
+    return (
+      <header className='header'>
+        <div>
+          <img src={LogoOne} alt='logo' className='header__logo' />
+          <a href='#!' className='header__link'>
+            Coffee house
+          </a>
+        </div>
+        <a href='#!' className='header__link'>
+          Our coffee
+        </a>
+        <a href='#!' className='header__link'>
+          Favorite / Basket
+        </a>
+      </header>
+    );
+  }
+}
+
+export { Header };
