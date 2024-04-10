028e244396309f40fe10885c4293196727531f20
Meow-Double <slava.pugachev.2015@mail.ru>
2024-04-10 13:44:50 +0300

    fix: corrected errors

--- src/components/index.js
+++ src/components/index.js
@@ -2,8 +2,5 @@ export * from './about/about';
 export * from './content/content';
 export * from './footer/footer';
 export * from './header/header';
-<<<<<<< HEAD
-// export * from './sectionTitle/sectionTitle';
-=======
 export * from './tittleHome/tittleHome';
->>>>>>> master
+
