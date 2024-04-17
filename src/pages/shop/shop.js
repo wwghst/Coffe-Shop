5f0c25beb674455b7ca2d8fe4221ddb596a1f4c6
Arslan <wowghost@bk.ru>
2024-04-17 19:10:18 +0500

    ShopPage is done

--- src/pages/shop/shop.js
+++ src/pages/shop/shop.js
@@ -1,1 +1,20 @@
+import { Component } from 'react';
+
+import ShopBg from '../../assets/images/ShopTittleBg.png';
+import { AboutShop, Footer, Header, ShopContent, Tittle } from '../../components';
+
 import './shop.scss';
+
+export class ShopPage extends Component {
+  render() {
+    return (
+      <div className='container'>
+        <Header />
+        <Tittle text='Our Coffee' alt='coffee' imgName={ShopBg} />
+        <AboutShop />
+        <ShopContent />
+        <Footer />
+      </div>
+    );
+  }
+}
